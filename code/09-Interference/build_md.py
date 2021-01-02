f6ec841dd04a6390fb49d7ab5d4fef2ce1fb669d
FEZ (Felix Kratzer) <felix@kratzer.org>
2021-01-02 11:41:49 +0100

    Added 09

--- code/09-Interference/build_md.py
+++ code/09-Interference/build_md.py
@@ -6,5 +6,5 @@ with open(f"md_{input_file}.txt", "w") as md:
     md.write(f"file 'output/{input_file}.png'\n")
     md.write(f"duration 2\n")
     for j in range(int(argv[2])):
-        md.write(f"file 'output/{input_file}.png_out_{j:03d}.png'\n")
+        md.write(f"file 'output/{input_file}.png_out_{j:04d}.png'\n")
         md.write(f"duration 0.1\n")
